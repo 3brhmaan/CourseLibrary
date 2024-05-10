2f66a000bc834ad4d26da5f4d5514a189944feed
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-05-11 00:46:53 +0300

    Building Custom Model Binding - building custom model binder to help in binding array key i.e. "1,2,3,4" to ienumerable type and this help in returning a collection of resources in one request - refactor the POST action to return a location header in the response containing the route to a collection of resources

--- CourseLibrary.API/Controllers/AuthorCollectionsController.cs
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using CourseLibrary.API.Entities;
+using CourseLibrary.API.Helpers;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Http;
@@ -15,25 +16,48 @@ public class AuthorCollectionsController : ControllerBase
     private readonly IMapper _mapper;
 
     public AuthorCollectionsController(
-        ICourseLibraryRepository courseLibraryRepository ,
+        ICourseLibraryRepository courseLibraryRepository,
         IMapper mapper)
     {
         _courseLibraryRepository = courseLibraryRepository;
         _mapper = mapper;
     }
 
+
+    [HttpGet("({authorIds})", Name = "GetAuthorCollection")]
+    public async Task<ActionResult<IEnumerable<AuthorForCreationDto>>> GetAuthorCollection(
+        [ModelBinder(BinderType = typeof(ArrayModelBinder))] [FromRoute]
+        IEnumerable<Guid> authorIds)
+    {
+        var authorEntities = await _courseLibraryRepository
+            .GetAuthorsAsync(authorIds);
+
+        // do we have all requested author
+        if (authorEntities.Count() != authorIds.Count())
+            return NotFound();
+
+        var authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+        return Ok(authorsToReturn);
+    }
+
+
     [HttpPost]
     public async Task<ActionResult<IEnumerable<AuthorDto>>> CreateAuthorCollection(
         IEnumerable<AuthorForCreationDto> authorCollection)
     {
         var authorEntities = _mapper.Map<IEnumerable<Author>>(authorCollection);
-        foreach(var author in authorEntities)
-        {
-            _courseLibraryRepository.AddAuthor(author);
-        }
+        foreach (var author in authorEntities) _courseLibraryRepository.AddAuthor(author);
 
         await _courseLibraryRepository.SaveAsync();
 
-        return Ok();
+        var authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+
+        var authorIds = string.Join(",",
+            authorCollectionToReturn.Select(x => x.Id));
+
+        return CreatedAtRoute(
+            "GetAuthorCollection",
+            new { authorIds },
+            authorCollectionToReturn);
     }
 }
---
+++ CourseLibrary.API/Helpers/ArrayModelBinder.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CourseLibrary.API.Helpers;
+
+public class ArrayModelBinder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        // our binder works only on enumerable types
+        if (!bindingContext.ModelMetadata.IsEnumerableType)
+        {
+            bindingContext.Result = ModelBindingResult.Failed();
+            return Task.CompletedTask;
+        }
+
+        // get the inputted value through the value provider
+        var value = bindingContext.ValueProvider
+            .GetValue(bindingContext.ModelName)
+            .ToString();
+
+        // if that value is null or white space , we return null
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            bindingContext.Result = ModelBindingResult.Success(null);
+            return Task.CompletedTask;
+        }
+
+        // the value isn't null or white space ,
+        // and the type of the model is enumerable 
+        // get the Enumerable's type , and a converter
+        var elementType = bindingContext.ModelType
+            .GetTypeInfo()
+            .GenericTypeArguments[0];
+
+        var converter = TypeDescriptor.GetConverter(elementType);
+
+        // Convert Each item in teh value list to enumerable type
+        var values = value.Split(
+                new [] {","}, 
+                StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => converter.ConvertFromString(x.Trim()))
+            .ToArray();
+
+        // create an array of that type and set it as the model value
+        var typedValues = Array.CreateInstance(elementType, values.Length);
+        values.CopyTo(typedValues , 0);
+        bindingContext.Model = typedValues;
+
+        // return a successful result , passing in the model 
+        bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+        return Task.CompletedTask;
+    }
+}
