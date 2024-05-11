b32a570cd00ce70be848a6a19e82225bd3ac4365
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-05-11 06:31:04 +0300

    Upserting Using PUT request enable updating and inserting using PUT request

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -97,7 +97,17 @@ public class CoursesController : ControllerBase
 
         if (courseForAuthorFromRepo == null)
         {
-            return NotFound();
+            var courseToAdd = _mapper.Map<Course>(course);
+            courseToAdd.Id = courseId;
+            _courseLibraryRepository.AddCourse(authorId , courseToAdd);
+            await _courseLibraryRepository.SaveAsync();
+
+            var courseToReturn = _mapper.Map<CourseDto>(courseToAdd);
+
+            return CreatedAtRoute(
+                "GetCourseForAuthor" ,
+                new {authorId , courseId = courseToReturn.Id} ,
+                courseToReturn);
         }
 
         _mapper.Map(course, courseForAuthorFromRepo);
