83139fd6cfbf693b47a40f082b01489afa4561a9
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-05-16 01:56:56 +0300

    suporting filtering and searching on Author Entity

--- CourseLibrary.API/Services/CourseLibraryRepository.cs
+++ CourseLibrary.API/Services/CourseLibraryRepository.cs
@@ -120,7 +120,33 @@ public class CourseLibraryRepository : ICourseLibraryRepository
 #pragma warning restore CS8603 // Possible null reference return.
     }
 
-   
+    public async Task<IEnumerable<Author>> GetAuthorsAsync(string? mainCategory , string? searchQuery)
+    {
+        if (string.IsNullOrWhiteSpace(mainCategory) && string.IsNullOrWhiteSpace(searchQuery))
+            return await GetAuthorsAsync();
+
+        var collection = _context.Authors as IQueryable<Author>;
+
+        if (!string.IsNullOrWhiteSpace(mainCategory))
+        {
+            mainCategory = mainCategory.Trim();
+            collection = collection.Where(a => a.MainCategory == mainCategory);
+        }
+
+        if (!string.IsNullOrWhiteSpace(searchQuery))
+        {
+            searchQuery = searchQuery.Trim();
+            collection = collection
+                .Where(a => 
+                        a.MainCategory.Contains(searchQuery) ||
+                        a.FirstName.Contains(searchQuery) ||
+                        a.LastName.Contains(searchQuery)
+                    );
+        }
+
+        return await collection.ToListAsync();
+    }
+
     public async Task<IEnumerable<Author>> GetAuthorsAsync()
     {
         return await _context.Authors.ToListAsync();
