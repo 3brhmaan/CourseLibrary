1cd11e47935b10d59929f246779a29d5d3e0d3a8
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-05-15 03:56:36 +0300

    Custom validation using IValidatableObject

--- CourseLibrary.API/Models/CourseForManipulationDto.cs
+++ CourseLibrary.API/Models/CourseForManipulationDto.cs
@@ -2,8 +2,19 @@
 
 namespace CourseLibrary.API.Models;
 
-public abstract class CourseForManipulationDto
+public abstract class CourseForManipulationDto : IValidatableObject
 {
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Title == Description)
+        {
+            yield return new ValidationResult(
+                "the provided description should be different from the title",
+                new[] { "Course" , "Test" }
+            );
+        }
+    }
+
     [Required(ErrorMessage = "you should fill out the title")]
     [MaxLength(100, ErrorMessage = "the title shouldn't have more than 100 character")]
     public string Title { get; set; } = string.Empty;
