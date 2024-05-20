42416aeedc8381dafe251841d35d795fcc710bc2
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-05-20 09:41:57 +0300

    implementing HATEOAS support after POSTing

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -226,9 +226,16 @@ public class AuthorsController : ControllerBase
 
         var authorToReturn = _mapper.Map<AuthorDto>(authorEntity);
 
+        var links = CreateLinksForAuthor(authorToReturn.Id, null);
+        
+        var linkedResourceToReturn = authorToReturn.ShapeData(null) 
+            as IDictionary<string, object?>;
+
+        linkedResourceToReturn.Add("links" , links);
+
         return CreatedAtRoute("GetAuthor",
-            new { authorId = authorToReturn.Id },
-            authorToReturn);
+            new { authorId = linkedResourceToReturn["Id"] },
+            linkedResourceToReturn);
     }
 
 
